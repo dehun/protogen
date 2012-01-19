67de5839433da12f06c2d9f45e55f60e9253b559
Yuriy Netesov <973520+dehun@users.noreply.github.com>
2012-01-19 14:28:04 +0200

    [messaging::message_loader] loader implementation

--- messaging/message_loader.py
+++ messaging/message_loader.py
@@ -1,6 +1,25 @@
+import imp
+from inspect import isclass
+import sys
+from message import pgMessage
+
 class MessageLoader:
     def __init__(self, path):
         self._messages_path = path
 
+    def _load_mod(self):
+        mod = imp.load_source(re.search('/(\w+?)\.py', self._messages_path).group(1), self._messages_path)
+        return mod
+
+    def _load_all_messages(self, mod):
+        return filter(lambda msg: pgMessage is not msg, \ # remove pgMessage itself
+                      [mod.__dict__[key] for key in \ # retrive value from dict by a filtered keys to a list
+                       filter(lambda k: issubclass(mod.__dict__[k], pgMessage), \ # check is msg keyed is subclass of pgMessage
+                              filter(lambda k: isclass(mod.__dict__[k]), mod.__dict__.keys()))]) # filter for classes
+        
     def get_all(self):
-        raise NotImplementedError()
+        mod = self._load_mod()
+        messages = _load_all_messages(mod)
+        del mod
+        return messages
+
