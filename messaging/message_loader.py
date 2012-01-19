deff215d529e27313fe3c792247be106afafae5d
Yuriy Netesov <retnuhed@gmail.com>
2012-01-19 15:04:43 +0200

    add __init__.py

--- messaging/message_loader.py
+++ messaging/message_loader.py
@@ -1,6 +1,6 @@
 import imp
+import re
 from inspect import isclass
-import sys
 from message import pgMessage
 
 class MessageLoader:
@@ -12,14 +12,14 @@ class MessageLoader:
         return mod
 
     def _load_all_messages(self, mod):
-        return filter(lambda msg: pgMessage is not msg, \ # remove pgMessage itself
-                      [mod.__dict__[key] for key in \ # retrive value from dict by a filtered keys to a list
-                       filter(lambda k: issubclass(mod.__dict__[k], pgMessage), \ # check is msg keyed is subclass of pgMessage
-                              filter(lambda k: isclass(mod.__dict__[k]), mod.__dict__.keys()))]) # filter for classes
+        return filter(lambda msg: pgMessage is not msg, \
+                      [mod.__dict__[key] for key in \
+                       filter(lambda k: isclass(mod.__dict__[k]) and issubclass(mod.__dict__[k], pgMessage), \
+                              mod.__dict__.keys())])
         
     def get_all(self):
         mod = self._load_mod()
-        messages = _load_all_messages(mod)
+        messages = self._load_all_messages(mod)
         del mod
         return messages
 
