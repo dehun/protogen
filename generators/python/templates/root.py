c24113fdeeaaa92f8d28f4938f54840a21040f08
dehun <retnuhed@gmail.com>
2012-02-17 18:41:14 +0000

    logger and some templates

--- generators/python/templates/root.py
+++ generators/python/templates/root.py
@@ -1,10 +1,13 @@
-from protogen.generators.template import Template
+from protogen.generators.templates.template import Template
+from protogen.generators.python.templates.messages import Messages
+from protogen.generators.python.templates.deserialize_factory import DeserializeFactory
 
 class Root(Template):
 	def __init__(self, messages):
 		self._messages = messages
 
 	def body(self):
-		return """ root body """
+		self.add(Messages(self._messages))
+		self.add(DeserializeFactory(self._messages))
 		
 		
