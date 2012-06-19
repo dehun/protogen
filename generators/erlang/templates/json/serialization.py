5df9a7bbb2556625797b251df2cc83804ca7fad6
Yuriy Netesov <973520+dehun@users.noreply.github.com>
2012-06-19 15:38:39 +0000

    [erlang::json] module name fix

--- generators/erlang/templates/json/serialization.py
+++ generators/erlang/templates/json/serialization.py
@@ -11,7 +11,7 @@ class MessagesSerialization(Template):
     def body(self):
         protocolName = self._protocol.get_name()
         self.add(TSimple(StringTemplate("-module(${protocolName}_json_messaging).").substitute(locals())))
-        self.add(TSimple(StringTemplate('-include("${protocol_name}_messaging.hrl").').substitute(locals())))
+        self.add(TSimple(StringTemplate('-include("${protocolName}_messaging.hrl").').substitute(locals())))
         self.add(TSimple("-export([serialize_message/1, deserialize_message/1]).")) 
         self.add(MessageSerializers(self._protocol))
         self.add(MessageDeserializers(self._protocol))
