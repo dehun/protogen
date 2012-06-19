db703780ba8fa4ea7912bf035dddab3d71d8acd5
Yuriy Netesov <973520+dehun@users.noreply.github.com>
2012-06-19 14:55:49 +0000

    [erlang::json] module name fix

--- generators/erlang/templates/serialization.py
+++ generators/erlang/templates/serialization.py
@@ -1,6 +1,7 @@
 from protogen.generators.templates.template import Template, TSimple
 from protogen.generators.erlang.templates.serializers import MessageSerializers
 from protogen.generators.erlang.templates.deserializers import MessageDeserializers
+from string import Template as StringTemplate
 
 class MessagesSerialization(Template):
     def __init__(self, protocol):
@@ -9,7 +10,7 @@ class MessagesSerialization(Template):
 
     def body(self):
         code = ""
-        code += "-module(messaging).\n"
+        code += StringTemplate("-module(${protoname}_json_messaging).\n").substitute({'protoname' : self._protocol.get_name()})
         code += "-export([serialize_message/1, deserialize_message/1]).\n"
         self.add(MessageSerializers(self._protocol))
         self.add(MessageDeserializers(self._protocol))
